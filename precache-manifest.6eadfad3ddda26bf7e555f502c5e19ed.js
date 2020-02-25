d5cd6b4dd7a09a4c564d144cc26dc72e785023ab
pdione <dione.pape@gmail.com>
2020-02-25 22:09:19 +0100

    Updates

---
+++ precache-manifest.6eadfad3ddda26bf7e555f502c5e19ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27e93c15adad937e034e927a08d87005",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b44df794683feb029987",
+    "url": "/monsters-rolodex/static/css/main.b13f72d9.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b44df794683feb029987",
+    "url": "/monsters-rolodex/static/js/main.b41f00e0.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
